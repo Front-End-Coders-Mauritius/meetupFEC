1ba62303a9a58c3e56101a365154d149811613e5
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-13 16:07:53 +0400

    cover description inprogress

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,8 +1,21 @@
 <template>
   <Header />
-  <div class="main-container grid grid-cols-2 bg-red-300 bg-cover bg-center">
-    <div></div>
-    <div></div>
+  <div class="main-container bg-cover bg-center">
+    <div class="main-cover bg-center bg-cover" style="height: 700px">
+      <div
+        class="flex flex-col justify-center items-end gap-4 w-full h-full bg-gradient-to-l from-white p-8 shadow-xl"
+      >
+        <div class="text-right">
+          <h1 class="text-4xl md:text-6xl font-extrabold">Front-end Coders</h1>
+          <h1 class="text-4xl md:text-6xl font-extrabold">Mauritius</h1>
+        </div>
+        <p class="text-md md:text-xl w-96 font-bold text-right leading-5">
+          Front-End coders Mauritius is a free meetup that's dedicated to all
+          aspect of Front-End development. it occurs in general one a month at
+          announced date and place. Feel free to join!
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -14,9 +27,7 @@ export default {
 };
 </script>
 <style scoped>
-.main-container {
+.main-cover {
   background-image: url("../assets/cover.jpeg");
-  background-size: fit;
-  height: 700px;
 }
 </style>
