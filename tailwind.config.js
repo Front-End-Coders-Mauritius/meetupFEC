2fb7926c3456a337f821ed712653986b91520778
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-13 14:01:16 +0400

    upd Header + font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: [],
   theme: {
+    fontFamily: {
+      Pacifico: ["Pacifico", "open-sans"],
+      Mochiy: ['"Mochiy Pop P One"', "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
