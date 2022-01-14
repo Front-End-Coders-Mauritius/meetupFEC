62abe85425b59f5b8699776a51ccf09182980666
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-14 22:42:54 +0400

    updated responsive homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,8 +1,8 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <div class="banner-container relative bg-gray-50 overflow-hidden">
+  <div class="banner-container relative bg-red-50 overflow-hidden">
     <div
-      class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
+      class="hidden sm:block sm:absolute sm:inset-y-0 max-h-7xl sm:h-full sm:w-full"
       aria-hidden="true"
     >
       <div class="relative h-full max-w-7xl mx-auto">
@@ -136,74 +136,82 @@
       </main>
     </div>
   </div>
-  <div
-    class="latest-events-container md:max-w-2xl lg:max-w-7xl mx-auto my-8 py-8"
-  >
-    <div class="text-5xl font-bold py-8">
-      <h1 class="bg-gradient-to-r from-blue-200 p-4">Latest events</h1>
-    </div>
+  <div class="latest-events-container bg-gray-50">
     <div
-      class="rounded-lg bg-gray-200 overflow-hidden shadow-xl divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
+      class="latest-events-wrapper md:max-w-2xl lg:max-w-7xl mx-auto py-8 px-4 md:px-0"
     >
       <div
-        v-for="(event, eventIdx) in events"
-        :key="event.title"
-        :class="[
-          eventIdx === 0
-            ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none'
-            : '',
-          eventIdx === 1 ? 'sm:rounded-tr-lg' : '',
-          eventIdx === events.length - 2 ? 'sm:rounded-bl-lg' : '',
-          eventIdx === events.length - 1
-            ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none'
-            : '',
-          'relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500',
-        ]"
+        class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl py-8"
       >
-        <div>
+        <h1 class="">Latest events</h1>
+      </div>
+      <div
+        class="rounded-lg bg-gray-200 overflow-hidden shadow-xl divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px"
+      >
+        <div
+          v-for="(event, eventIdx) in events"
+          :key="event.title"
+          :class="[
+            eventIdx === 0
+              ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none'
+              : '',
+            eventIdx === 1 ? 'sm:rounded-tr-lg' : '',
+            eventIdx === events.length - 2 ? 'sm:rounded-bl-lg' : '',
+            eventIdx === events.length - 1
+              ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none'
+              : '',
+            'relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500',
+          ]"
+        >
+          <div>
+            <span
+              :class="[
+                event.iconBackground,
+                event.iconForeground,
+                'rounded-lg inline-flex p-3 ring-4 ring-white',
+              ]"
+              ><ClockIcon class="h-6 w-6 mr-2" />
+              <span>{{ event.date }}, 9:30 Am</span>
+            </span>
+          </div>
+          <div class="mt-6">
+            <h3 class="text-lg font-medium">
+              <a :href="event.href" class="focus:outline-none">
+                <!-- Extend touch target to entire panel -->
+                <span class="absolute inset-0" aria-hidden="true" />
+                {{ event.title }}
+              </a>
+            </h3>
+            <p class="mt-2 text-sm text-gray-500">
+              Doloribus dolores nostrum quia qui natus officia quod et dolorem.
+              Sit repellendus qui ut at blanditiis et quo et molestiae.
+            </p>
+          </div>
           <span
-            :class="[
-              event.iconBackground,
-              event.iconForeground,
-              'rounded-lg inline-flex p-3 ring-4 ring-white',
-            ]"
-            ><ClockIcon class="h-6 w-6 mr-2" />
-            <span>{{ event.date }}, 9:30 Am</span>
+            class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
+            aria-hidden="true"
+          >
+            <svg
+              class="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
+              />
+            </svg>
           </span>
         </div>
-        <div class="mt-6">
-          <h3 class="text-lg font-medium">
-            <a :href="event.href" class="focus:outline-none">
-              <!-- Extend touch target to entire panel -->
-              <span class="absolute inset-0" aria-hidden="true" />
-              {{ event.title }}
-            </a>
-          </h3>
-          <p class="mt-2 text-sm text-gray-500">
-            Doloribus dolores nostrum quia qui natus officia quod et dolorem.
-            Sit repellendus qui ut at blanditiis et quo et molestiae.
-          </p>
-        </div>
-        <span
-          class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400"
-          aria-hidden="true"
-        >
-          <svg
-            class="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z"
-            />
-          </svg>
-        </span>
       </div>
     </div>
   </div>
 </template>
-
+<style scoped>
+.banner-container {
+  height: 600px;
+}
+</style>
 <script>
 import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import { MenuIcon, XIcon } from "@heroicons/vue/outline";
