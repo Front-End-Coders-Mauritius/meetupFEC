71a6bd150ef8b3294e24a8c4352857d721211339
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-14 21:46:39 +0400

    new homepage added

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,59 +1,166 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <Header />
-  <div class="main-container bg-cover bg-center">
-    <div class="main-cover bg-center bg-cover h-full">
-      <div
-        class="main-wrapper flex flex-col w-full h-full bg-gradient-to-t from-white p-8"
-      >
-        <div
-          class="main-details p-8 md:p-20 lg:p-30 flex flex-col justify-end items-end gap-4 w-full h-full"
+  <div class="relative bg-gray-50 overflow-hidden">
+    <div
+      class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full"
+      aria-hidden="true"
+    >
+      <div class="relative h-full max-w-7xl mx-auto">
+        <svg
+          class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+          width="404"
+          height="784"
+          fill="none"
+          viewBox="0 0 404 784"
         >
-          <div class="text-right bg-gradient-to-l from-yellow-300">
-            <h1 class="text-4xl md:text-5xl font-extrabold font-FiraSans">
-              Front-end Coders
-            </h1>
-            <h1 class="text-4xl md:text-5xl font-extrabold font-FiraSans">
-              <span class="">Mauritius </span>
-            </h1>
-          </div>
+          <defs>
+            <pattern
+              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="784"
+            fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+          />
+        </svg>
+        <svg
+          class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+          width="404"
+          height="784"
+          fill="none"
+          viewBox="0 0 404 784"
+        >
+          <defs>
+            <pattern
+              id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="784"
+            fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)"
+          />
+        </svg>
+      </div>
+    </div>
+
+    <div class="relative pt-6 pb-16 sm:pb-24">
+      <Popover>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6">
+          <nav
+            class="relative flex items-center justify-between sm:h-10"
+            aria-label="Global"
+          >
+            <div class="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span class="sr-only">Workflow</span>
+                <img
+                  class="h-8 w-auto sm:h-10"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt=""
+                />
+              </a>
+            </div>
+
+            <div class="md:flex md:space-x-10">
+              <a href=""> All Events </a>
+            </div>
+          </nav>
+        </div>
+      </Popover>
+
+      <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+        <div class="text-center">
+          <h1
+            class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+          >
+            <span class="block xl:inline">Front-end Coders</span>
+            {{ " " }}
+            <span class="block text-indigo-600 xl:inline">Mauritius</span>
+          </h1>
           <p
-            class="text-md md:text-xl font-bold w-96 text-right leading-5 font-FiraSans"
+            class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
           >
             Front-End coders Mauritius is a free meetup that's dedicated to all
             aspect of Front-End development. it occurs in general one a month at
             announced date and place. Feel free to join!
           </p>
+          <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+            <div class="rounded-md shadow">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                All events
+              </a>
+            </div>
+            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              >
+                Gallery
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="event-container p-10 md:p-20">
-    <div class="events-wrapper md:w-1/2 flex flex-col gap-4 justify-center p-8">
-      <h1 class="text-5xl font-extrabold">
-        <span class="text-black font-FiraSans bg-gradient-to-r from-red-500 p-2"
-          >Latest Events</span
-        >
-      </h1>
-
-      <Events />
+      </main>
     </div>
   </div>
 </template>
+
 <script>
-import Header from "./Header.vue";
-import Events from "./Events.vue";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { MenuIcon, XIcon } from "@heroicons/vue/outline";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
 export default {
   components: {
-    Header,
-    Events,
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    return {
+      navigation,
+    };
   },
 };
 </script>
-<style scoped>
-.main-container {
-  height: 800px;
-}
-.main-cover {
-  background-image: url("../assets/websiteCover.jpeg");
-}
-</style>
