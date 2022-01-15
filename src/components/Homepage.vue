18f5425818ffca7894b12793319aeba0d14bfc7a
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-15 23:52:36 +0400

    updated homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -12,7 +12,7 @@
               class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl"
             >
               <span class="block xl:inline">Front-End Coders</span>
-
+              {{ " " }}
               <span class="block text-indigo-600 xl:inline">Mauritius</span>
             </h1>
             <p
