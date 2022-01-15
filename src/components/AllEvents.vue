528d61a2bd3a24317f5361034c717e5da103bbe3
Mahima Ramgolam <mahima7691487@gmail.com>
2022-01-15 13:57:46 +0400

    allevents updated

--- src/components/AllEvents.vue
+++ src/components/AllEvents.vue
@@ -1,64 +1,109 @@
 <template>
   <Header />
-  <div class="allEvents-container py-4 mx-auto">
-    <div
-      class="allEvents-header my-4 w-full h-16 flex justify-center items-center bg-gradient-to-r from-red-600 rounded-md"
-    >
-      <h1 class="text-3xl font-extrabold font-FiraSans px-4 py-2 text-red-900">
-        All Events 2020
-      </h1>
-    </div>
-    <div class="allEvents-list flex flex-wrap justify-center gap-8 py-4">
-      <router-link
-        to=""
-        v-for="event in eventlists"
-        :key="event"
-        class="h-64 w-72 flex flex-col justify-between items-center px-4 py-8 rounded-lg shadow-xl"
-      >
-        <div
-          class="text-md text-red-500 font-bold bg-gradient-to-r from-red-200 p-2 rounded-md"
-        >
-          SAT 09, OCTOBER 2020, 9:30 Am
-        </div>
-        <div class="text-2xl text-center font-bold text-red-900">
-          Front-end Coders October Meetup 2020
-        </div>
-        <div class="flex gap-2 text-md text-red-200">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-            /></svg
-          ><span>Bocasay</span>
-        </div>
-      </router-link>
-    </div>
+  <div class="bg-white shadow overflow-hidden sm:rounded-md">
+    <ul role="list" class="divide-y divide-gray-200">
+      <li v-for="position in positions" :key="position.id">
+        <a href="#" class="block hover:bg-gray-50">
+          <div class="px-4 py-4 sm:px-6">
+            <div class="flex items-center justify-between">
+              <p class="text-sm font-medium text-indigo-600 truncate">
+                {{ position.title }}
+              </p>
+              <div class="ml-2 flex-shrink-0 flex">
+                <p
+                  class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                >
+                  {{ position.type }}
+                </p>
+              </div>
+            </div>
+            <div class="mt-2 sm:flex sm:justify-between">
+              <div class="sm:flex">
+                <p class="flex items-center text-sm text-gray-500">
+                  <UsersIcon
+                    class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  {{ position.department }}
+                </p>
+                <p
+                  class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6"
+                >
+                  <LocationMarkerIcon
+                    class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  {{ position.location }}
+                </p>
+              </div>
+              <div class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0">
+                <CalendarIcon
+                  class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                <p>
+                  Closing on
+                  {{ " " }}
+                  <time :datetime="position.closeDate">{{
+                    position.closeDateFull
+                  }}</time>
+                </p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
 import Header from "./Header.vue";
+import {
+  CalendarIcon,
+  LocationMarkerIcon,
+  UsersIcon,
+} from "@heroicons/vue/solid";
+const positions = [
+  {
+    id: 1,
+    title: "Back End Developer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Engineering",
+    closeDate: "2020-01-07",
+    closeDateFull: "January 7, 2020",
+  },
+  {
+    id: 2,
+    title: "Front End Developer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Engineering",
+    closeDate: "2020-01-07",
+    closeDateFull: "January 7, 2020",
+  },
+  {
+    id: 3,
+    title: "User Interface Designer",
+    type: "Full-time",
+    location: "Remote",
+    department: "Design",
+    closeDate: "2020-01-14",
+    closeDateFull: "January 14, 2020",
+  },
+];
 export default {
   components: {
+    CalendarIcon,
+    LocationMarkerIcon,
+    UsersIcon,
     Header,
   },
 
   data: () => {
     return {
       eventlists: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      positions,
     };
   },
 };
